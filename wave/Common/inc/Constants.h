8d0b446bbb1ec81a3ed5f1c0dc2a66e70d16cb04
Ed-Visinheski <eduardo.cook-visinheski@city.ac.uk>
2026-03-27 19:26:19 +0000

    Fixed issue with solo harmonics not appearing due to a small buffer size, increased to twice the previous size

--- wave/Common/inc/Constants.h
+++ wave/Common/inc/Constants.h
@@ -6,5 +6,5 @@ namespace AudioConstants
     constexpr int   SAMPLE_RATE      = 44100;
     constexpr int   MAX_HARMONICS    = 16; //Currently unused maybe be helpful to allow user to decide on number of harmonics
     constexpr int   NUM_HARMONICS    = 5;
-    constexpr size_t BUFFER_SIZE = 4096;
+    constexpr size_t BUFFER_SIZE = 8192;
 }
